6b08700aa0161d7c07ee23274a6935a4ef21966a
Raza <rmqazi@hotmail.com>
2016-11-08 17:36:08 -0700

    Feature completed TCPServer. Clients can request for objects in root directory. Uses regex to determine if GET request is well formed.
    Added comments

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,29 +1,38 @@
+/**
+ * @author  Raza Qazi
+ * @version 1.0, 11/4/2016
+ */
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.SocketTimeoutException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 
 public class WebServer extends Thread {
 
-
     private volatile boolean shutdown = false;
-    ExecutorService executor;
-    Socket socket;
-    ServerSocket sc = null;
-    int portNum;
+    private Socket socket;
+    private ServerSocket sc = null;
+    private int portNum;
 
+    /**
+     * Default constructor to initialize port number
+     *
+     * @param port
+     */
     public WebServer(int port) {
         // Initialization
         portNum = port;
 
     }
 
+    /**
+     * Establishes a TCP connection to client by waiting for
+     * a client to accept a connection. Spawns new worker threads
+     * to handle multiple GET requests simultaneously
+     *
+     * Note: overrides run() from Thread
+     */
     public void run() {
         // Open server socket
         try {
@@ -36,8 +45,10 @@ public class WebServer extends Thread {
         }
         while (!shutdown) {
             try {
+                // Wait for a client to accept, else timeout
                 socket = sc.accept();
 
+                // Spawn and start a new thread to handle connection
                 Thread tcpCon = new socketThread(socket);
                 tcpCon.start();
 
@@ -49,9 +60,15 @@ public class WebServer extends Thread {
         }
     }
 
+    /**
+     * When executes, stop the Main thread from accepting connections
+     * and closes all sockets.
+     */
     public void shutdown() {
+        // Flag to stop the while loop
         shutdown = true;
         try {
+            // Close all sockets
             sc.close();
             socket.close();
         } catch (IOException e) {
--- src/socketThread.java
+++ src/socketThread.java
@@ -1,7 +1,11 @@
+/**
+ * @author  Raza Qazi
+ * @version 1.0, 11/4/2016
+ */
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.*;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.regex.Pattern;
@@ -10,14 +14,26 @@ public class socketThread extends Thread {
     // Internal socket
     private Socket socket;
     // String pattern checker - assume that all files must be in root directory
-    private String pattern = "(GET)( )(\\/)([a-zA-Z]+)(\\.|)([a-zA-Z]+)( )(HTTP)(\\/)(1\\.0|1\\.1)";
+    private String pattern = "(GET)( )(\\/)([a-zA-Z0-9]+)(\\.|)([a-zA-Z0-9]+)( )(HTTP)(\\/)(1\\.0|1\\.1)";
     // format for date
     private SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss zzz");
 
+    /**
+     * Default constructor to initialize socket passed in from WebServer
+     *
+     * @param sc
+     */
     public socketThread(Socket sc) {
             socket = sc;
     }
 
+    /**
+     * Worker thread responsible for receiving header information
+     * from client, interpreting the data, and sending a response
+     * back with a file (if required)
+     *
+     * Note: overrides run() from Thread
+     */
     public void run() {
         try {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -31,6 +47,7 @@ public class socketThread extends Thread {
             String date = generateCurrentDate();
 
             // Flag true if header is well formed
+            // Uses regex to determine validity of header content.
             boolean wellFormed = Pattern.matches(pattern, header);
 
             if (wellFormed) {
@@ -46,7 +63,7 @@ public class socketThread extends Thread {
                     Path path = Paths.get(objFile[0]);
                     byte[] data = new byte[0];
                     try {
-                        // COnvert file to byte array
+                        // Convert file to byte array
                         data = Files.readAllBytes(path);
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -59,12 +76,15 @@ public class socketThread extends Thread {
                             "Last-Modified: " + sdf.format(file.lastModified())+ "\r\n" +
                             "Content-Length: " + data.length + "\r\n" +
                             "Connection: closed\r\n";
+
                     // Send to client
-                    out.println(response);
+                    out.print(response);
+                    out.println();
                     System.out.println("Sending out: " + objFile[0]);
 
                     // Send object to client
                     socket.getOutputStream().write(data);
+                    System.out.println("Sent: "+ objFile[0]);
                 } else {
                     // File not found - 404 Not Found
                     String response =
@@ -94,6 +114,11 @@ public class socketThread extends Thread {
         }
     }
 
+    /**
+     * Generates current date using standard format clients expect
+     *
+     * @return String of Current date
+     */
     public String generateCurrentDate() {
         //SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd hh:mm:ss zzz yyyy");
         Date d = new Date();
